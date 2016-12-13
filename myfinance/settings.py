0a8a83b238d8dfe52ee53543b52329828e480aed
Ilyua Chumak <old.flanigan@gmail.com>
2016-12-14 01:26:32 +0300

    Work real mail

--- myfinance/settings.py
+++ myfinance/settings.py
@@ -29,14 +29,16 @@ ALLOWED_HOSTS = []
 
 ACCOUNT_ACTIVATION_DAYS = 2 # 
 
-
 AUTH_USER_EMAIL_UNIQUE = True
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = 'info@google.ru'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'                                                                             
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'old.flanigan@gmail.com'
+EMAIL_HOST_PASSWORD = 'bwllgntvfrpbbgbc'
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL =  'old.flanigan@gmail.com'
+
 
 # Application definition
 
